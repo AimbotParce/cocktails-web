797120c291d72cbc6d8f6a14bec6b4d61c74310e
AimbotParce <marcparcerisa@gmail.com>
2024-03-28 00:49:09 +0100

    Home page doesn't need to be a client component

--- app/cocktails/[uuid]/page.tsx
+++ app/cocktails/[uuid]/page.tsx
@@ -2,5 +2,14 @@ import get_cocktail from "@/api/cocktails/uuid/get"
 
 export default async function Page({ params }: { params: { uuid: string } }) {
     const cocktail = await get_cocktail(params.uuid)
-    return <p>Post: {cocktail.name}</p>
+    return (
+        <main>
+            <h1>{cocktail.name}</h1>
+            <ul>
+                {cocktail.ingredients.map((ingredient) => (
+                    <li key={ingredient.uuid}>{ingredient.name}</li>
+                ))}
+            </ul>
+        </main>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-"use client"
 import get_cocktails from "@/api/cocktails/get"
-import Cocktail from "@/api/models/cocktail"
 import CocktailCard from "@/components/CocktailCard"
-import { useEffect, useState } from "react"
 
-export default function Home() {
-    const [cocktails, setCocktails] = useState<Cocktail[]>([])
-
-    useEffect(() => {
-        get_cocktails().then(setCocktails)
-    }, [])
+export default async function Home() {
+    const cocktails = await get_cocktails()
 
     return (
         <main className="flex flex-col gap-4">
