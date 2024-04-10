701ccbdfa886cfc330e73a9240c720b8b7f99629
AimbotParce <marcparcerisa@gmail.com>
2024-04-11 00:41:56 +0200

    Ingredients list on cocktails card

--- components/CocktailCard.tsx
+++ components/CocktailCard.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion"
 
 const CocktailCard = ({ id, uuid, name, instructions, ingredients, image, creation_datetime }: Cocktail) => {
     const src = `${process.env.NEXT_PUBLIC_API_URL}/attachments/images/${image.uuid}`
+    const ingredientNames = ingredients.map((ingredient) => ingredient.name).join(", ")
     return (
         <motion.a
             className="flex gap-4 p-2 bg-white items-center cursor-pointer"
@@ -13,11 +14,7 @@ const CocktailCard = ({ id, uuid, name, instructions, ingredients, image, creati
             <img src={src} alt={name} width={100} height={100} />
             <div>
                 <h1 className="font-bold">{name}</h1>
-                <ul className="text-gray-500">
-                    {ingredients.map((ingredient) => (
-                        <li key={ingredient.id}>{ingredient.name}</li>
-                    ))}
-                </ul>
+                <ul className="text-gray-500">{ingredientNames}</ul>
             </div>
         </motion.a>
     )
